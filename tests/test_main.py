8b3289f8c8e6077d3b8084a9a73609830ee61f08
Ivan Synytskyi <ivansinickij29@icloud.com>
2026-03-30 16:40:54 +0200

    done pytest

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,7 +10,7 @@ from app.players.dwarves.dwarf import Dwarf
 from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
 from app.players.dwarves.dwarf_warrior import DwarfWarrior
 from app.players.elves.druid import Druid
-from app.players.elves.elf import Elf
+from app.players.elves.elf_ranger import Elf
 from app.players.elves.elf_ranger import ElfRanger
 from app.players.player import Player
 
